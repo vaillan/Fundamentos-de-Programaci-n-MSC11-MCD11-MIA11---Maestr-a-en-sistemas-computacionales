815c810f0b136bd6b186421f0c9514ab1db0fb15
vaillan <red.line.v.o.s@outlook.com>
2023-09-09 18:45:33 -0600

    1 file modified

--- index.js
+++ index.js
@@ -21,12 +21,16 @@ function onInit(e) {
 
 /**
  * Función para calcular el factorial
+ * El factorial de un número es equivalente a la multiplicación de todos los números enteros 
+ * positivos menores o iguales que él.
  * 
  * @return {void}
  */
 function factorial(inputValue) {
   result = 1;
+  //Bucle para genear serie de numeros empesando desde el 1 hasta llegar al numero de entrada 
   for (let i = 1; i <= inputValue; i++) {
+    //Calculo para gegeral factorial
     result = result * i;
   }
 }
